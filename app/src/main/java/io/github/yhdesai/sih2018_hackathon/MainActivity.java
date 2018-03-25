0d5a75a3f332b2d787984b1cc5c2b80b95163e07
Yash <yhdesai99@ymail.com>
2018-03-25 12:09:46 +0530

    works other than networkOnMainThread crash

--- app/src/main/java/io/github/yhdesai/sih2018_hackathon/MainActivity.java
+++ app/src/main/java/io/github/yhdesai/sih2018_hackathon/MainActivity.java
@@ -23,6 +23,8 @@ import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
 
+import static java.lang.System.out;
+
 public class MainActivity extends Activity {
 
     private TextView txtSpeechInput;
@@ -94,19 +96,32 @@ public class MainActivity extends Activity {
                     input = result.get(0);
                     //Here, input string contains the text of what the user spoke
 
-                    sourceLang = "en";
-                    targetLang = "hi";
+                    sourceLang = "hi";
+                    targetLang = "en";
+                    Translate translate = createTranslateService();
+                    Translate.TranslateOption srcLang = Translate.TranslateOption.sourceLanguage(sourceLang);
+                    Translate.TranslateOption tgtLang = Translate.TranslateOption.targetLanguage(targetLang);
+                    String sourceText = input;
 
-                    translate.translateTextWithOptions(input, sourceLang, targetLang, translatedText);
-                    txtSpeechOutput.setText(translatedText);
+
+
+                  //  translate.translateTextWithOptions(input, sourceLang, targetLang, translatedText);
+
 
                   //  executeNLP(translatedText);
                   //  String action = result.getString
 
                     // continue translating here
-                    Translate translate = createTranslateService();
+                    translate = createTranslateService();
                     Translation translation = translate.translate(input);
                     txtSpeechOutput.setText(translation.getTranslatedText());
+                    Translate.TranslateOption model = Translate.TranslateOption.model("nmt");
+                    translation = translate.translate(sourceText, srcLang, tgtLang, model);
+                    out.printf("Source Text:\n\tLang: %s, Text: %s\n", sourceLang, sourceText);
+                    out.printf("TranslatedText:\n\tLang: %s, Text: %s\n", targetLang,
+                            translation.getTranslatedText());
+                    txtSpeechOutput.setText( translation.getTranslatedText());
+
 
 
                 }
@@ -116,56 +131,10 @@ public class MainActivity extends Activity {
         }
     }
 
-    public static void translateTextWithOptions(
-            String sourceText,
-            String sourceLang,
-            String targetLang,
-            PrintStream out) {
-
-        Translate translate = createTranslateService();
-        Translate.TranslateOption srcLang = Translate.TranslateOption.sourceLanguage(sourceLang);
-        Translate.TranslateOption tgtLang = Translate.TranslateOption.targetLanguage(targetLang);
-
-        // Use translate `model` parameter with `base` and `nmt` options.
-        Translate.TranslateOption model = Translate.TranslateOption.model("nmt");
-
-        Translation translation = translate.translate(sourceText, srcLang, tgtLang, model);
-        out.printf("Source Text:\n\tLang: %s, Text: %s\n", sourceLang, sourceText);
-        out.printf("TranslatedText:\n\tLang: %s, Text: %s\n", targetLang,
-                translation.getTranslatedText());
-    }
     public static Translate createTranslateService() {
         return TranslateOptions.newBuilder().build().getService();
     }
 
 
-/*
-    public static void main(String[] args) {
-        String command = args[0];
-        String text;
 
-        if (command.equals("detect")) {
-            text = args[1];
-            translate.detectLanguage(text, System.out);
-        } else if (command.equals("translate")) {
-            text = args[1];
-            try {
-                String sourceLang = args[2];
-                String targetLang = args[3];
-                translate.translateTextWithOptions(text, sourceLang, targetLang, translatedText);
-            } catch (ArrayIndexOutOfBoundsException ex) {
-                translate.translateText(text, System.out);
-            }
-        } else if (command.equals("langsupport")) {
-            try {
-                String target = args[1];
-                //TranslateText.displaySupportedLanguages(System.out, Optional.of(target));
-                // TranslateText.displaySupportedLanguages(System.out, Optional.of(target));
-            } catch (ArrayIndexOutOfBoundsException ex) {
-                // TranslateText.displaySupportedLanguages(System.out, Optional.empty());
-            }
-        }
-    }
-
-*/
 }
